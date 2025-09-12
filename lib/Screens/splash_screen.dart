970d1e34c8c47564337474183d9804b7233c0c8c
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-12 22:03:14 +0530

    Added Splash Screen

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({super.key});
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/counting.png", height: 150, width: 150),
+            SizedBox(height: 35),
+            Text(
+              "Countify – Every Launch Counts.",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
