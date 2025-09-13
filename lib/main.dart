59bec12f66e388a739f179d8bd5c2218398aaf69
Sagar Dhadke <peaceunique2003@gmail.com>
2025-09-13 17:31:08 +0530

    Fix: Use initialRoute instead of home to prevent splash screen from staying in back stack

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:countify/Routes/app_routes.dart';
-import 'package:countify/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      home: MySplashScreen(),
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.splash_screen,
       routes: AppRoutes.mRoutes,
